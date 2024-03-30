b5451ba01fd98acca155c5c72a44f0f38514ce36
Nate <natewillis1@gmail.com>
2024-03-30 06:15:38 -0500

    moving asgi around some more

--- centurion/centurion/asgi.py
+++ centurion/centurion/asgi.py
@@ -7,23 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
 import os
+from django.core.asgi import get_asgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "centurion.settings.production")
+django_asgi_app = get_asgi_application()
 
+# Now do the rest
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
 from chat.routing import websocket_urlpatterns
 
-
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "centurion.settings.production")
-
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
-django_asgi_app = get_asgi_application()
-
-
+# Setup the application
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
