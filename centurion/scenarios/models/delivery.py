73c4d169c577b5a73b1c54c7e10e2f13b0c6a813
Nate <natewillis1@gmail.com>
2024-04-03 15:18:44 -0500

    added location to delivery

--- centurion/scenarios/models/delivery.py
+++ centurion/scenarios/models/delivery.py
@@ -1,11 +1,12 @@
-from django.db import models
+from django.contrib.gis.db import models
+from django.contrib.gis.geos import Point
 from .pickup import Pickup
 
 class Delivery(models.Model):
     weight = models.IntegerField(blank=False, null=False)
     pickup = models.ForeignKey(Pickup, on_delete=models.CASCADE, related_name='deliveries')
     offset = models.DurationField(blank=False, null=False)
-    #location = models.PointField(blank=False, null=False, srid=4326)
+    location = models.PointField(blank=False, null=False, srid=4326, default=Point(-95.9, 41.2))
     altitude_ft = models.FloatField(blank=False, null=False)
 
     def __str__(self):
