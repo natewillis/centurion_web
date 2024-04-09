b7e8e4e03b7723bc7a9cfcbc7b965ce071cfc0df
Nate <natewillis1@gmail.com>
2024-04-09 04:55:08 -0500

    finished greenwich sidereal time and ecef to geo

--- centurion/common/tests.py
+++ centurion/common/tests.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 from scenarios.models import Scenario, Order, Pickup, Delivery
 from .utilities.general_utilities import get_all_related_objects
-from .utilities.astrodynamic_utilities import convert_ECEF_to_geodetic, convert_geodetic_to_ECEF
-
+from .utilities.astrodynamic_utilities import convert_ECEF_to_geodetic, convert_geodetic_to_ECEF, greenwich_sidereal_time
+import datetime
 
 class GetAllRelatedObjectsTestCase(TestCase):
     def setUp(self):
@@ -37,9 +37,29 @@ class ECEFtoGeodeticConversionsTestCase(TestCase):
         self.lat = 43
         self.alt = 0
     
+    def test_convert_geodetic_to_ECEF(self):
+        r1 = convert_geodetic_to_ECEF(self.lat, self.lon, self.alt)
+        print(f'r1 is {r1 }')
+        self.assertAlmostEqual(r1[0], -131.27138439572073, 0)
+        self.assertAlmostEqual(r1[1], -2519.196, 0)
+        self.assertAlmostEqual(r1[2], 2336.6641469, 0)
+
+    def test_convert_ECEF_to_geodetic(self):
+        converted_lat, converted_lon, converted_alt = convert_ECEF_to_geodetic([-132.025378, -2519.196, 2336.661469])
+        print(f'return is is {converted_lat}, {converted_lon}, {converted_alt}')
+        self.assertAlmostEqual(self.lat, converted_lat , 0)
+        self.assertAlmostEqual(self.lon, converted_lon, 0)
+        self.assertAlmostEqual(self.alt, converted_alt, 0)
+
     def test_both(self):
         r1 = convert_geodetic_to_ECEF(self.lat, self.lon, self.alt)
         converted_lat, converted_lon, converted_alt = convert_ECEF_to_geodetic(r1)
-        self.assertAlmostEqual(self.lat, converted_lat)
-        self.assertAlmostEqual(self.lon, converted_lon)
-        self.assertAlmostEqual(self.alt, converted_alt)
+        self.assertAlmostEqual(self.lat, converted_lat, 0)
+        self.assertAlmostEqual(self.lon, converted_lon, 0)
+        self.assertAlmostEqual(self.alt, converted_alt, 0)
+
+class GreenwichSiderealTimeTestCase(TestCase):
+    def test_greenwich_sidereal_time(self):
+        time_to_convert = datetime.datetime(year=2004, month=3, day=3, hour=4, minute=30)
+        converted_time = greenwich_sidereal_time(time_to_convert)
+        self.assertAlmostEqual(converted_time, 228.79354,3)
--- centurion/common/utilities/astrodynamic_utilities.py
+++ centurion/common/utilities/astrodynamic_utilities.py
@@ -1,5 +1,5 @@
 from math import atan2, asin, acos, sin, cos, sqrt
-
+import datetime
 
 RADIUS_EARTH_NM = 3443.92
 DEGREES_TO_RADIANS = .01745
@@ -9,56 +9,32 @@ E2 = 0.00669437999014
 def convert_ECEF_to_geodetic(r1):
 
     a = RADIUS_EARTH_NM
-    
-    a1 = 23.0549091356429
-    a2 = 531.528835252753
-    a3 = 0.077169161196072
-    a4 = 1328.82208813188
-    a5 = 23.132078296839
-    a6 = 0.99330562000986
+    f = 0.00335281066
 
     # do the conversion
     x = r1[0]
     y = r1[1]
     z = r1[2]
 
-    zp = abs(z)
-    w2 = x*x + y*y
-    w = sqrt(w2)
-    r2 = w2 + z*z
-    r = sqrt(r2)
-    lon = atan2(y, x)
-
-    s2 = z * z / r2
-    c2 = w2 / r2
-    u = a2 / r
-    v = a3 - a4 / r
-
-    if c2 > 0.3:
-        s = (zp / r) * (1 + c2 * (a1 + u + s2 * v) / r)
-        lat = asin(s)
-        ss = s ** s
-        c = sqrt(1 - ss)
-    else:
-        c = (w / r) * (1  - s2 * (a5 - u - c2 * v) / r)
-        lat = acos(c)
-        ss = 1 - c * c
-        s = sqrt(ss)
-
-    g = 1 - E2 * ss
-    rg = a / sqrt( g)
-    rf = a6 * rg
-    u = w - rg * c
-    v = zp - rf * s
-    f = c * u + s * v
-    m = c * v - s * u
-    p = m / (rf / g + f)
-    lat = lat + p
-    alt = f + m * p / 2
-    if z < 0:
-        lat = lat * -1
+    # derived constants
+    b = a - f * a
+    clambda = atan2(y, x)
+    p = sqrt(x * x + y * y)
+    h_old = 0 # first guess is 0 altitude
+    theta = atan2(z, p * (1-E2))
+    cs = cos(theta)
+    sn = sin(theta)
+    n = (a * a) / sqrt((a * cs) ** 2 + (b * sn) ** 2)
+    h = p / cs - n
+    while (h - h_old) > 1.0e-6:
+        h_old = h
+        theta = atan2(z, p * (1-E2 * n / (n + h)))
+        cs = cos(theta)
+        sn = sin(theta)
+        n = (a * a) / sqrt((a * cs) ** 2 + (b * sn) ** 2)
+        h = p / cs - n
     
-    return lat/DEGREES_TO_RADIANS, lon/DEGREES_TO_RADIANS, alt
+    return  theta/DEGREES_TO_RADIANS, clambda/DEGREES_TO_RADIANS, h
 
 
 # From Converting between ECEF and Geodetic coordinates, D. Rose - November 2014
@@ -78,3 +54,23 @@ def convert_geodetic_to_ECEF(lat_degrees, lon_degrees, alt_nm):
 
     # return
     return [iprime, jprime, kprime]
+
+def greenwich_sidereal_time(datetime_to_convert):
+
+    # get the julian day start
+    julian_day_0h0m = 367 * datetime_to_convert.year - int((7 / 4) * (datetime_to_convert.year + int((datetime_to_convert.month + 9) / 12))) + int(275 * datetime_to_convert.month / 9) + datetime_to_convert.day + 1721013.5
+    print(f'j0={julian_day_0h0m}')
+    # calculate month start
+    no_time = datetime.datetime(datetime_to_convert.year, datetime_to_convert.month, datetime_to_convert.day) 
+    dif_of_datetime = datetime_to_convert - no_time
+    ut_dec = dif_of_datetime.total_seconds()/datetime.timedelta(days=1).total_seconds()
+
+    # calc julian centuries
+    julian_centuries_j2000 = (julian_day_0h0m - 2451545) / 36525
+    print(f't0={julian_centuries_j2000}')
+
+    # get theta
+    theta_g0 = 100.4606184 + 36000.77004 * julian_centuries_j2000 + 0.000387933 * julian_centuries_j2000 ** 2 - 0.00000002583 * julian_centuries_j2000 ** 3
+
+    # finish up
+    return (theta_g0 + 360.98564724 * ut_dec) % 360
